0bc4db3880e5195d1d052481d00a010f38792eed
MuhummadShohag <muhammadshohagislam.software.e@gmail.com>
2022-05-15 22:24:37 +0300

    Fixed Configaration Problem

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+// const nextConfig = {
+//     reactStrictMode: true,
+// };
 
-module.exports = nextConfig
+// module.exports = nextConfig;
+
+module.exports = {
+    reactStrictMode: true,
+    webpack5: true,
+    webpack(config) {
+        config.resolve.fallback = {
+            ...config.resolve.fallback,
+            fs: false,
+        };
+        return config;
+    },
+};
